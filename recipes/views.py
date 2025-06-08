128f826db1397b168b718bfbfe8ea618e12df2cf
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-06-08 15:59:31 -0300

    Corrigido os arquivos static e criado a pagina recipe

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context ={
-        'name': 'Rafael',
-    })
+    return render(request, 'recipes/pages/home.html')
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
