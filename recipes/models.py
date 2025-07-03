c35da6177af286738b0e7be7a5266ebf44e61179
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-07-03 00:58:49 -0300

    Corrigido os erros de tipo

--- recipes/models.py
+++ recipes/models.py
@@ -1,12 +1,14 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class Recipe(models.Model):
@@ -16,7 +18,7 @@ class Recipe(models.Model):
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_unit = models.CharField(max_length=65)   
+    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,11 +27,15 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
 
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
-from django.http import Http404
-from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
 from recipes.models import Recipe
 
 
@@ -9,15 +8,15 @@ def home(request):
         is_published=True
     ).order_by('-id')
 
-    
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
+
 def category(request, category_id):
     recipes = get_list_or_404(
         Recipe.objects.filter(
-            category__id=category_id, 
+            category__id=category_id,
             is_published=True,
         ).order_by('-id')
     )
@@ -28,6 +27,7 @@ def category(request, category_id):
     }
 
     return render(request, 'recipes/pages/category.html', context)
+
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
