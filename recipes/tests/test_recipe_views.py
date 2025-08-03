3a71a69d3cf06e234d8182c02da9acca0bf18168
Rafael Pereira <rafaelpereiradearaujo5@gmail.com>
2025-08-03 14:55:58 -0300

    test: create base test class with recipe fixture

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,12 +1,13 @@
-from django.test import TestCase
 from django.urls import resolve, reverse
-
 from recipes import views
-from recipes.models import Category, Recipe, User
+
+from .test_recipe_base import RecipeTestBase
 
 
-# checa se os caminhos das URLs estão apontando para as views corretas.
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
+    def tearDown(self) -> None:
+        return super().tearDown()
+
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(
             reverse("recipes:home")
@@ -29,31 +30,10 @@ class RecipeViewsTest(TestCase):
         )
 
     def test_recipe_home_templates_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='user',
-            username='user',
-            password='123456',
-            email='username@email.com'
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time='10',
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
+
         self.assertIn('Recipe Title', content)
         self.assertIn('10 Minutos', content)
         self.assertIn('5 Porções', content)
