e1dd0d6a02e42877db993cb281f420ec540c3445
Rafael Pereira <rafaelpereiradearaujo5@gmail.com>
2025-08-25 02:32:23 -0300

    feat(browser): implement make_chrome_browser function for headless browsing

---
+++ utils/browser.py
@@ -0,0 +1,40 @@
+# from pathlib import Path
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+
+# ROOT_PATH = Path(__file__).parent.parent
+# CHROMEDRIVER_NAME = ''
+# CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
+
+
+# def make_chrome_browser(*options):
+#     chrome_options = webdriver.ChromeOptions()
+#     chrome_service = Service(exutable_path=CHROMEDRIVER_PATH)
+#     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+#     return browser
+
+
+# if __name__ == '__main__':
+#     browser = make_chrome_browser()
+#     browser.get('http://www.udemy.com.br/')
+#     browser.quit()
+
+from selenium import webdriver
+
+
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    browser = webdriver.Chrome(options=chrome_options)
+    return browser
+
+
+if __name__ == '__main__':
+    browser = make_chrome_browser('--headless')
+    browser.get('http://www.udemy.com.br/')
+    browser.quit()
