d47d7052689b4bc50a68ba982de41c0e358b919c
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-09-09 00:10:15 -0300

    refactor(authors views): replace dashboard recipe functions with DashboardRecipe class-based view

--- authors/urls.py
+++ authors/urls.py
@@ -13,12 +13,12 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name='dashboard'),
     path(
         "dashboard/recipe/new/",
-        views.dashboard_recipe_new,
+        views.DashboardRecipe.as_view(),
         name="dashboard_recipe_new"
     ),
     path(
         "dashboard/recipe/delete/",
-        views.dashboard_recipe_edit,
+        views.dashboard_recipe_delete,
         name='dashboard_recipe_delete'
     ),
     path(
