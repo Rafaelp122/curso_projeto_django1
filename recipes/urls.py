77182c023f048cc875287d30262eef4399b7133f
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-09-19 11:36:17 -0300

    refactor(recipes): rename RecipeListView to RecipeListViewBase and update related views for better clarity and structure

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = "recipes"
 
 urlpatterns = [
-    path("", views.RecipeListView.as_view(), name="home"),
+    path("", views.RecipeListViewHome.as_view(), name="home"),
     path('recipes/search/', views.search, name="search"),
     path(
         "recipes/category/<int:category_id>/",
-        views.CategoryListView.as_view(),
+        views.RecipeListViewCategory.as_view(),
         name="category"
     ),
     path("recipes/<int:id>/", views.recipe, name="recipe"),
--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ import os
 from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 
 from recipes.models import Recipe
@@ -12,15 +12,15 @@ from utils.pagination import make_pagination
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
-class RecipeListView(ListView):
+class RecipeListViewBase(ListView):
     model = Recipe
     context_object_name = 'recipes'
     paginate_by = None
     ordering = ['-id']
-    template_name = 'recipes/pages/home.html'
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
+
         filtered_qs = qs.filter(
             is_published=True,
         )
@@ -39,60 +39,30 @@ class RecipeListView(ListView):
         return ctx
 
 
-class CategoryListView(ListView):
-    model = Recipe
-    context_object_name = 'recipes'
-    paginate_by = None
-    ordering = ['-id']
+class RecipeListViewHome(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewCategory(RecipeListViewBase):
     template_name = 'recipes/pages/category.html'
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
-        category_id = self.kwargs.get('category_id')
+
         filtered_qs = qs.filter(
-            category__id=category_id,
-            is_published=True,
+            category__id=self.kwargs.get('category_id'),
         )
         return filtered_qs
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
+
         recipes_list = self.object_list
         if recipes_list:
             category_name = recipes_list.first().category.name
             ctx['title'] = f'{category_name} - Category | '
 
-        page_obj, pagination_range = make_pagination(
-            self.request,
-            ctx.get('recipes'),
-            PER_PAGE
-        )
-        ctx.update(
-            {
-                'recipes': page_obj,
-                'pagination_range': pagination_range,
-            }
-        )
         return ctx
-
-
-def category(request, category_id):
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            category__id=category_id,
-            is_published=True,
-        ).order_by("-id")
-    )
-
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    context = {
-        "recipes": page_obj,
-        "pagination_range": pagination_range,
-        "title": f"{recipes[0].category.name} - Category | "
-    }
-
-    return render(request, "recipes/pages/category.html", context)
 
 
 def recipe(request, id):
