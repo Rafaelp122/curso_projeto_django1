e743701cd9f26dbf7daf4a0e339375cd66646636
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-11-16 13:06:02 -0300

    fix(settings): load environment variables from .env file

--- project/settings.py
+++ project/settings.py
@@ -14,10 +14,12 @@ import os
 from pathlib import Path
 
 from django.contrib.messages import constants
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
