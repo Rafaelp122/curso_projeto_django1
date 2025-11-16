6e651d55c9b71770a327ce628a3e38e1520610ac
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-11-16 13:35:06 -0300

    refactor(recipes): replace function-based recipe view with class-based detail view

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 import os
-
+from django.views.generic import DetailView, ListView
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
@@ -68,19 +68,18 @@ class RecipeListViewCategory(RecipeListViewBase):
         return ctx
 
 
-def recipe(request, id):
-    recipe = get_object_or_404(
-        Recipe,
-        pk=id,
-        is_published=True,
-    )
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/pages/recipe-view.html'
 
-    context = {
-        "recipe": recipe,
-        "is_detail_page": True,
-    }
-
-    return render(request, "recipes/pages/recipe-view.html", context)
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+    
+        ctx.update({
+            'is_detail_page': True
+        })
+        return ctx
 
 
 class RecipeListViewSearch(RecipeListViewBase):
