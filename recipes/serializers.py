e92b13bc338733fcc3654e383c5557c538a3e265
Rafael <rafaelpereiradearaujo5@gmail.com>
2026-01-11 20:36:14 -0300

    refactor: extract recipe validation logic to reusable validator class
    - Create AuthorRecipeValidator class to centralize validation rules
    - Refactor AuthorRecipeForm to use new validator
    - Upgrade RecipeSerializer from Serializer to ModelSerializer
    - Add validation support to serializer using AuthorRecipeValidator
    - Implement POST method support in recipe API endpoint
    - Add Tag model migration with many-to-many relationship
    - Clean up code formatting and remove redundant validation methods

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,39 +1,58 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
+from tag.models import Tag
 
-from .models import Tag
+from authors.validators import AuthorRecipeValidator
 
-
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_lenght=255)
-    slug = serializers.SlugField()
+from .models import Recipe
 
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField()
-    category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug']
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description', 'author',
+            'category', 'tags', 'public', 'preparation',
+            'tag_objects', 'tag_links',
+            'preparation_time', 'preparation_time_unit', 'servings',
+            'servings_unit',
+            'preparation_steps', 'cover'
+        ]
+
+    public = serializers.BooleanField(
+        source='is_published',
+        read_only=True,
     )
-    tags = serializers.PrimaryKeyRelatedField(
-        queryset=Tag.objects.all(),
-        many=True,
+    preparation = serializers.SerializerMethodField(
+        method_name='any_method_name',
+        read_only=True,
+    )
+    category = serializers.StringRelatedField(
+        read_only=True,
     )
     tag_objects = TagSerializer(
-        many=True,
-        source='tags'
+        many=True, source='tags',
+        read_only=True,
     )
     tag_links = serializers.HyperlinkedRelatedField(
         many=True,
         source='tags',
-        queryset=Tag.objects.all(),
         view_name='recipes:recipes_api_v2_tag',
+        read_only=True,
     )
 
-    def get_preparation(self, recipe):
+    def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+
+    def validate(self, attrs):
+        super_validate = super().validate(attrs)
+        AuthorRecipeValidator(
+            data=attrs,
+            ErrorClass=serializers.ValidationError,
+        )
+        return super_validate
